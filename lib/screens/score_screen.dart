925d5cfb9cbff6f0f7366e833ba55ecc5c3ea642
Denis Luis Rybier <rybier97@gmail.com>
2023-11-23 17:25:35 -0300

    pantalla
    pantallazo

--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -17,31 +17,65 @@ class _ScoreboardScreenState extends State<ScoreboardScreen> {
   }
 
   Future<void> loadScores() async {
-    
     scores = await scoreManager.readScores();
-    setState(() {}); 
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tabla de Puntuaciones'),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Tabla de Puntuaciones',
+          style: TextStyle(
+            fontFamily: 'roundedsqure',
+            fontSize: 25,
+            color: Colors.white,
+          ),
+        ),
       ),
-      body: scores.isEmpty
-          ? const Center(
-              child: Text('No hay puntuaciones disponibles'),
-            )
-          : Center(child: ListView.builder(
-              itemCount: scores.length,
-              itemBuilder: (context, index) {
-                final score = scores[index];
-                return ListTile(
-                  title: Text(score.playerName),
-                  subtitle: Text('Puntos: ${score.score}'),
-                );
-              },
-            ),
-    ));
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, Color.fromARGB(90, 33, 149, 243)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.3, 0.9],
+          ),
+        ),
+        child: Center(
+        child: scores.isEmpty
+            ? Text('No hay puntuaciones disponibles')
+            : ListView.builder(
+                itemCount: scores.length,
+                itemBuilder: (context, index) {
+                  final score = scores[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 16.0),
+                      title: Center(
+                          child: Text(score.playerName,
+                              style: const TextStyle(
+                                fontFamily: 'roundedsqure',
+                                fontSize: 25,
+                                color: Colors.white,
+                              ))),
+                      subtitle: Center(
+                          child: Text('Puntos: ${score.score}',
+                              style: const TextStyle(
+                                fontFamily: 'roundedsqure',
+                                fontSize: 25,
+                                color: Colors.white,
+                              ))),
+                    ),
+                  );
+                },
+              ),
+      )),
+      backgroundColor: Colors.black,
+    );
   }
 }
